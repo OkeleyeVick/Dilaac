4c7227d15724e310abac5618e6ca566aac933d70
Vickkk <justusofnigeria@gmail.com>
2023-08-17 05:58:17 +0100

    updates

--- Admin/custom.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelectorAll(".nav-links > li > a");
-
-links.forEach((link) => {
-	if (link.pathname === window.location.pathname) {
-		link.classList.add("active");
-	}
-});
