7e3690f5938bb3167c20a12ae43522d7bb507723
Vickkk <justusofnigeria@gmail.com>
2023-08-18 01:46:32 +0100

    updates

--- assets/js/control.js
+++
@@ -1,8 +0,0 @@
-AOS.init();
-const links = document.querySelectorAll(".logo-et-nav-links .nav-links > li > a");
-
-links.forEach((link) => {
-	if (link.pathname === window.location.pathname) {
-		link.classList.add("active");
-	}
-});
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,42 @@
-const formFile = document.getElementById("formFile");
-const selectImage = document.querySelector("form button.select-image");
+const allCodeInputs = document.querySelectorAll("#v-input-pin-form-container .v-pins-container .form-control");
 
-selectImage.addEventListener("click", function (e) {
-  e.preventDefault();
-  e.stopPropagation();
-  formFile.click();
+function pasteCodes(e) {
+	/* document listens for a paste event,
+  if the element recieving it is not an input tag,
+  don't paste it 
+  */
+	if (e.target.localName !== "input") return;
+	// e.preventDefault();
+
+	// get the pincodes
+	let pinCodes = (e.clipboardData || window.clipboardData).getData("text");
+	/*
+    if the length of the code
+    is not the same as the the length 
+    of the input, do nothing
+    */
+	if (pinCodes.length !== allCodeInputs.length) return;
+
+	allCodeInputs.forEach((eachInput, index) => {
+		eachInput.focus();
+		eachInput.value = pinCodes[index];
+	});
+}
+
+// document or web browser listen for paste event
+document.addEventListener("paste", pasteCodes);
+
+allCodeInputs.forEach((input, index, arrayOfInputs) => {
+	input.addEventListener("input", function (event) {
+		if (event.target.value.length === 0) return;
+		arrayOfInputs[index + 1]?.focus();
+	});
+});
+
+allCodeInputs.forEach((input, index, arrayOfInputs) => {
+	input.addEventListener("input", function (e) {
+		if (e.inputType == "deleteContentBackward") {
+			arrayOfInputs[index - 1]?.focus();
+		}
+	});
 });
